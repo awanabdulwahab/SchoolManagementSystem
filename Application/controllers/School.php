564e577b41f5e6aff2c8b6e7763beb9daf02549d
awanabdulwahab7@outlook.com <awanabdulwahab7@outlook.com>
2020-07-08 11:32:22 +0500

    basic step up of header and footer file

---
+++ Application/controllers/School.php
@@ -0,0 +1,20 @@
+<?php
+
+
+class School extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function Index(){
+
+	}
+
+	public function category(){
+		$this->load->view('shared/header.php');
+		$this->load->view('shared/category.php');
+		$this->load->view('shared/footer.php');
+	}
+}
